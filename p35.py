b8d806318587e3a89a867e2bfe878411ac51e732
afarica <57631090+afarica@users.noreply.github.com>
2019-11-12 04:45:10 +0600

    Update p35.py

--- p35.py
+++ p35.py
@@ -1,6 +1,6 @@
+# Write the code, which will print numbers from 0 till your age. And if your age
+# is odd, will be printed all odd numbers till your age, if even all even numbers.
 a=int(input("How old are you:"))
 print(list(range(0,a+1)))
 if a%2==0: 
-	print(list(range(0,a+1,2)))
-	
-
+	print(list(range(0,a+1,2)))	
